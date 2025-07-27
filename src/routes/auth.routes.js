ea4f6a6398797cb5652a430ee941abcf3d857e95
Piyush <166606245+Piyush232005@users.noreply.github.com>
2025-07-27 22:56:32 +0530

    changes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,12 +8,33 @@ router.post('/register', async (req, res) => {
     // Registration logic here
     const { username, password } = req.body;
     const user = await userModel.create({
-        username,password
+        username, password
     })
-    res.status(201).json({ 
-        message: 'User registered successfully', 
-        user 
+    res.status(201).json({
+        message: 'User registered successfully',
+        user
     });
+})
+
+router.post('/login', async (req, res) => {
+    // Login logic here
+    const { username, password } = req.body;
+
+    const user = await userModel.findOne({
+        username: username,
+
+    })
+    if (!user) {
+        return res.status(401).json({
+            message: 'User not found'
+        });
+    }
+    const isPasswordvalid = password === user.password;
+    if(!isPasswordvalid) {
+        return res.status(401).json({
+            message: 'Invalid password'
+        });
+    }
 })
 
 module.exports = router;
