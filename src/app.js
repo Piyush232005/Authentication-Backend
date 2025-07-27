4f7bd8e1640100a5a14f141795c23e27e32cd3db
Piyush <166606245+Piyush232005@users.noreply.github.com>
2025-07-27 22:45:28 +0530

    Some changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const authRoutes = require('./routes/auth.routes');
+
 
 
 const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
 
+// post/auth/register
+// post/auth/login
+// post/auth/logout
+// post/auth/user
+
+app.use('/auth',authRoutes);
 
 module.exports = app;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const userModel = require('../models/user.model');
 
 const router = express.Router();
 
 
+router.post('/register', async (req, res) => {
+    // Registration logic here
+    const { username, password } = req.body;
+    const user = await userModel.create({
+        username,password
+    })
+    res.status(201).json({ 
+        message: 'User registered successfully', 
+        user 
+    });
+})
 
 module.exports = router;
